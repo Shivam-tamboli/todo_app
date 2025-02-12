4f1e9d695813ee53787d97d8e2c01fa23087cdbd
Shivam <shivamtamboli66@gmail.com>
2025-02-12 19:26:02 +0530

    Provides CRUD operations to the Task

--- src/main/java/com/app/todoapp/repository/TaskRepository.java
+++ src/main/java/com/app/todoapp/repository/TaskRepository.java
@@ -5,3 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository  extends JpaRepository<Task,Long> {
 }
+//JpaRepository<Task,Long>: provides Basic CRUD Operations for the Task class.
+//No need to write queries manually Spring data jpa handle it automatically.
