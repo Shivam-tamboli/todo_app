fce688f22b297c1ac67d41248009521cdbf872f5
Shivam <shivamtamboli66@gmail.com>
2025-02-12 19:30:05 +0530

    update controller class

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -1,7 +1,32 @@
 package com.app.todoapp.controller;
 
+import com.app.todoapp.models.Task;
+import com.app.todoapp.services.TaskService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@Controller//mark this class as a spring mvc controller.
+@RequestMapping("/tasks")
 public class TaskController {
+
+    //dependency injection
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
+    }
+
+    //an endpoint.
+    @GetMapping
+    public String getTasks(Model model ){//models pass data controller to view html.
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
 }
+//controller will have all the end points, methods here to handle users request related to task.
