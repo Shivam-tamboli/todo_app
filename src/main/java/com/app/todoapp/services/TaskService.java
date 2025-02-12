e30ae8f2a0cfe3f4e982ae580bc02c7ae507d24f
Shivam <shivamtamboli66@gmail.com>
2025-02-12 19:27:30 +0530

    create method fetch all tasks

--- src/main/java/com/app/todoapp/services/TaskService.java
+++ src/main/java/com/app/todoapp/services/TaskService.java
@@ -1,8 +1,23 @@
 package com.app.todoapp.services;
 
+import com.app.todoapp.models.Task;
+import com.app.todoapp.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
+
+@Service//marks as a spring service component.
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
 }
-//These class files are used to write business logic in a different layer,
+//These class files are used to write business logic in a different layer.
+//service layer acts as a bridge between Controller(which handles request) and the repo(which interacts with DB)
