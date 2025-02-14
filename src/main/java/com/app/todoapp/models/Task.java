8955105d03f18f3097596b63dd42d54c1c241580
Shivam <shivamtamboli66@gmail.com>
2025-02-14 23:33:47 +0530

    fix getters and setters

--- src/main/java/com/app/todoapp/models/Task.java
+++ src/main/java/com/app/todoapp/models/Task.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
 
 @Entity
-@Data
+//@Data
+//@Getter
+//@Setter
 public class Task {
 
     @Id
@@ -15,4 +19,28 @@ public class Task {
     private Long id;
     private String title;
     private boolean completed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
